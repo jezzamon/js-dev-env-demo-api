aa61b2a196509ff4622efc9c2708d7804514a2fe
jezzamon <jezzamondev@gmail.com>
2017-02-14 10:10:24 -0500

    adding questions routes

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ var cors = require('cors');
 
 // https://murmuring-garden-99473.herokuapp.com/
 const app = express();
+const questions = require('./questions.js');
+const logger = require('morgan');
+const jsonParser = require('body-parser').json;
+//c:/users/jerry/documents/qa-rest-api project 
 app.use(cors());
 
 app.set('port', (process.env.PORT || 5000));
+
+//configure middleware to give us colorful status codes
+app.use(logger("dev"));
+//When app receives a request this middleware will parse the request as JSON and make it accessible from the request's body property
+app.use(jsonParser());
+
+// point to question routes
+app.use("/questions",questions);
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
